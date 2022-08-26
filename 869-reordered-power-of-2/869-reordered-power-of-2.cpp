b0d1e8e6fdf096392f83db0c97274a09d7ebd03d
Nesar Ahammed Jony <nesarjonybd@gmail.com>
2022-08-26 22:29:30 +0600

    Time: 8 ms (35.03%), Space: 5.9 MB (96.61%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -13,7 +13,6 @@ public:
     bool isAchiveable(int a, int b) {
         string aa = std::to_string(a);
         string bb = std::to_string(b);
- 
         
         sort(aa.begin(), aa.end());
         sort(bb.begin(), bb.end());
