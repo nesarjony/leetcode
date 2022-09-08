0467b2f5beffb6dd1ecba19fc0912186d5354d86
Nesar Ahammed Jony <nesarjonybd@gmail.com>
2022-09-08 22:09:49 +0600

    Time: 69 ms, Memory: 14 MB - LeetHub

---
+++ unique-email-addresses/unique-email-addresses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        valid = {}
+        for email in emails:
+            email = email.split("@")
+            name = email[0]
+            domain = email[1]
+            
+            keep = name.split("+")[0]
+            keep = keep.replace(".", "")
+            key = keep + "@" + domain
+            valid[key] = 1
+        
+        return len(valid)
